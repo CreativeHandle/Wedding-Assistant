d5f661d37f77dec8c561cce4a0c7bf7251f1c189
eujohn <eujohnmagno@gmail.com>
2019-03-14 21:11:28 +0800

    calendar

--- app/src/main/java/ph/edu/ceu/weddingassistant/CalendarActivity.java
+++ app/src/main/java/ph/edu/ceu/weddingassistant/CalendarActivity.java
@@ -1,51 +1,43 @@
 package ph.edu.ceu.weddingassistant;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
-import java.util.Calendar;
+import org.threeten.bp.format.DateTimeFormatter;
 
-public class CalendarActivity extends AppCompatActivity{
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+public class CalendarActivity extends AppCompatActivity implements
+        OnDateSelectedListener{
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("EEE, d MMM yyyy");
+
+    MaterialCalendarView widget;
+
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        widget = (MaterialCalendarView) findViewById(R.id.calendarView);
 
-        MaterialCalendarView calendar = (MaterialCalendarView) findViewById(R.id.calendarView);
-
-        calendar.setCurrentDate(CalendarDay.today());
-        Calendar currentCal = Calendar.getInstance();
-        currentCal.clear(Calendar.HOUR_OF_DAY);
-        currentCal.clear(Calendar.MINUTE);
-        currentCal.clear(Calendar.SECOND);
-        currentCal.clear(Calendar.MILLISECOND);
-        calendar.setSelectedDate(CalendarDay.today());
-
-        Calendar startCal = (Calendar) currentCal.clone();
-        startCal.set(Calendar.DAY_OF_WEEK, startCal.getFirstDayOfWeek());
-
-        Calendar endCal = (Calendar) startCal.clone();
-        endCal.add(Calendar.WEEK_OF_YEAR, 1);
-        calendar.state().edit().setMinimumDate(
-                CalendarDay.from(
-                        startCal.get(Calendar.YEAR),
-                        startCal.get(Calendar.MONTH),
-                        startCal.get(Calendar.DAY_OF_MONTH)
-                )
-        ).commit();
-
-        String val =calendar.getCurrentDate().toString();
-        TextView text = (TextView) findViewById(R.id.currentDate);
-        text.setText(val);
+        widget.setOnDateChangedListener(this);
     }
+
+    @Override
+    public void onDateSelected(
+            @NonNull MaterialCalendarView widget,
+            @NonNull CalendarDay date,
+            boolean selected) {
+        final String text = String.format("%s is available", FORMATTER.format(date.getDate()));
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
 }
